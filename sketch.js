4780adfa6fae7908f6f7b6e3dabd667ff660c396
Jonathan Cohen <jon@joncohen.dev>
2022-07-08 17:46:26 -0400

    Format

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ let animate = true;
 const Direction = {
   OVER: 0,
   DOWN: 1,
-}
+};
 
 /**
  * A constant-thickness line which can travel horizontally or 45 degrees downwards.
@@ -31,7 +31,7 @@ class Rail {
     /**
      * @type {Array.<{i: number, j: number}>}
      */
-    this.verticesGrid = [{i: 0, j}];
+    this.verticesGrid = [{ i: 0, j }];
 
     this.gridSizePixels = gridSizePixels;
     this.segmentSize = segmentSize;
@@ -40,13 +40,13 @@ class Rail {
   }
 
   /**
-   * 
+   *
    * @param {number} i Grid coordinate
    * @returns {number} Pixel coordinate
    */
- gridToPixel(i) {
-  return i * this.gridSizePixels;
-}
+  gridToPixel(i) {
+    return i * this.gridSizePixels;
+  }
 
   /**
    * Adds a new point in the rail at the grid point (x, y).  (0,0) is in the top left corner.
@@ -57,12 +57,13 @@ class Rail {
   addSegment(direction) {
     const lastEndpoint = this.verticesGrid[this.verticesGrid.length - 1];
     this.verticesGrid.push({
-      i: lastEndpoint.i + this.segmentSize, 
-      j: lastEndpoint.j + (direction === Direction.OVER ? 0: this.segmentSize)});
+      i: lastEndpoint.i + this.segmentSize,
+      j: lastEndpoint.j + (direction === Direction.OVER ? 0 : this.segmentSize),
+    });
   }
 
   /**
-   * 
+   *
    * @param {number} floorX The furthest left horizontal point to draw
    * @param {number} ceilingX The furthest right horizontal point to draw
    */
@@ -71,15 +72,15 @@ class Rail {
      * Returns the corresponding pixel value for `i`, but clamped to the range [startX, endX].
      * So if `gridToPixel(i)` < startX, you get startX, if `gridToPixel(i)` > endX you get endX, otherwise you get
      * `gridToPixel(i)`.
-     * @param {number} startX 
-     * @param {number} endX 
+     * @param {number} startX
+     * @param {number} endX
      * @param {number} i
      * @returns {number} Pixel coordinate
      */
     const clampedGridToPixel = (i) => {
       return max(min(this.gridToPixel(i), ceilingX), floorX);
-    }
-    
+    };
+
     const firstVertex = floor(floorX / this.gridSizePixels / this.segmentSize);
     const lastVertex = ceil(ceilingX / this.gridSizePixels / this.segmentSize);
 
@@ -87,20 +88,29 @@ class Rail {
       const startVertex = this.verticesGrid[vertex];
       const endVertex = this.verticesGrid[vertex + 1];
 
-      const deltaY = this.gridToPixel(endVertex.j) - this.gridToPixel(startVertex.j);
+      const deltaY =
+        this.gridToPixel(endVertex.j) - this.gridToPixel(startVertex.j);
 
-      const startX =  clampedGridToPixel(startVertex.i);
+      const startX = clampedGridToPixel(startVertex.i);
       // The slope is 1
-      const startY = this.gridToPixel(startVertex.j) + (startX - this.gridToPixel(startVertex.i)) * deltaY / this.stepSize;
+      const startY =
+        this.gridToPixel(startVertex.j) +
+        ((startX - this.gridToPixel(startVertex.i)) * deltaY) / this.stepSize;
       const endX = clampedGridToPixel(endVertex.i);
-      const endY = this.gridToPixel(endVertex.j) - (this.gridToPixel(endVertex.i) - endX) * deltaY / this.stepSize;
+      const endY =
+        this.gridToPixel(endVertex.j) -
+        ((this.gridToPixel(endVertex.i) - endX) * deltaY) / this.stepSize;
 
       fill(this.h, this.s, this.b);
       quad(
-        startX,                                                 startY,
-        startX - this.railThickness * tan(PI / 8), startY + this.railThickness,
-        endX - this.railThickness * tan(PI / 8),   endY + this.railThickness,
-        endX,                                                   endY
+        startX,
+        startY,
+        startX - this.railThickness * tan(PI / 8),
+        startY + this.railThickness,
+        endX - this.railThickness * tan(PI / 8),
+        endY + this.railThickness,
+        endX,
+        endY
       );
     }
   }
@@ -111,7 +121,16 @@ class Rail {
  * its owner can coordinate multiple RailBundles to not intersect
  */
 class RailBundle {
-  constructor(startI, bundleHeight, speed, flipVertical, flipHorizontal, gridSizePixels, segmentSize, railThickness) {
+  constructor(
+    startI,
+    bundleHeight,
+    speed,
+    flipVertical,
+    flipHorizontal,
+    gridSizePixels,
+    segmentSize,
+    railThickness
+  ) {
     this.flipVertical = flipVertical;
     this.flipHorizontal = flipHorizontal;
     const numRails = random(5, 15);
@@ -119,16 +138,23 @@ class RailBundle {
     this.startFrame = frameCount;
     this.rails = [];
     // The distance between the top and bottom rail, in grid units
-    const h = random(0,255);
-    const s = random(0,255);
-    const v = random(0,255);
+    const h = random(0, 255);
+    const s = random(0, 255);
+    const v = random(0, 255);
     // The bundle height in grid units from 1 to 1/4 of the screen
     this.bundleHeight = bundleHeight;
     for (let i = 0; i < numRails; ++i) {
-      this.rails.push(new Rail(
-        h, s, v, 
-        startI + i/numRails * this.bundleHeight + randomGaussian(0, 0.05), 
-        gridSizePixels, segmentSize, railThickness));
+      this.rails.push(
+        new Rail(
+          h,
+          s,
+          v,
+          startI + (i / numRails) * this.bundleHeight,
+          gridSizePixels,
+          segmentSize,
+          railThickness
+        )
+      );
     }
     this.done = false;
   }
@@ -136,7 +162,7 @@ class RailBundle {
   /**
    * Adds a new segment to each rail in the bundle.
    * TODO maybe sometimes we'll skip individual rail segments?
-   * @param {Direction} direction 
+   * @param {Direction} direction
    */
   addSegment(direction) {
     for (const rail of this.rails) {
@@ -150,14 +176,14 @@ class RailBundle {
     const floor = animate ? ceiling - windowWidth : 0;
     if (floor > windowWidth) {
       this.done = true;
-    };
+    }
     for (const rail of this.rails) {
       push();
       if (this.flipVertical) {
         translate(0, windowHeight);
         scale(1, -1);
       }
-      if(this.flipHorizontal) {
+      if (this.flipHorizontal) {
         translate(windowWidth, 0);
         scale(-1, 1);
       }
@@ -172,20 +198,29 @@ function newBundle() {
   const segmentSize = Math.random() * 5 + 1;
   const railThickness = gridSizePixels / random(1.5, 15);
   const speed = random(5, 15);
-  const startI = random(0, windowHeight / gridSizePixels * .9);
+  const startI = random(0, (windowHeight / gridSizePixels) * 0.9);
   const bundleHeight = random(1, floor(windowHeight / gridSizePixels / 4));
   const flipVertical = random(0, 1) < 0.5;
   const flipHorizontal = random(0, 1) < 0.5;
-  const bundle = new RailBundle(startI, bundleHeight, speed, flipVertical, flipHorizontal, gridSizePixels, segmentSize, railThickness);
+  const bundle = new RailBundle(
+    startI,
+    bundleHeight,
+    speed,
+    flipVertical,
+    flipHorizontal,
+    gridSizePixels,
+    segmentSize,
+    railThickness
+  );
   // The number of segments which fit horizontally on the screen
   for (let i = 0; i < ceil(windowWidth / gridSizePixels / segmentSize); ++i) {
-    bundle.addSegment(random() < .5 ? Direction.OVER : Direction.DOWN);
+    bundle.addSegment(random() < 0.5 ? Direction.OVER : Direction.DOWN);
   }
   return bundle;
 }
 
 let bundles = [];
-setInterval(() => bundles = bundles.filter(b => !b.done), 5000);
+setInterval(() => (bundles = bundles.filter((b) => !b.done)), 5000);
 let bundleFactoryInterval = null;
 
 function reset() {
@@ -194,13 +229,18 @@ function reset() {
   backgroundV = random(0, 255);
   clearInterval(bundleFactoryInterval);
   bundles = [];
-  if (animate) { bundleFactoryInterval = setInterval(() => (random(0, 1) < 0.1) && bundles.push(newBundle()), 50);}
+  if (animate) {
+    bundleFactoryInterval = setInterval(
+      () => random(0, 1) < 0.1 && bundles.push(newBundle()),
+      50
+    );
+  }
   bundles.push(newBundle());
 }
 
 function toggleAnimate() {
   animate = !animate;
-  if(!animate) reset();
+  if (!animate) reset();
 }
 
 function setup() {
@@ -213,7 +253,6 @@ function setup() {
 function doSave() {
   save(`${Date.now()}.png`);
 }
-
 
 function draw() {
   const speed = 10;
